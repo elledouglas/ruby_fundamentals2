67f7ca97e7dbad2a3a7f60fc6d075ad6ac591289
Kathleen <kathleenmcclay@gmail.com>
2017-06-27 14:46:25 -0400

    Almost done exercise 10

--- exercise10.rb
+++ exercise10.rb
@@ -3,3 +3,34 @@ students = {
   :cohort2 => 42,
   :cohort3 => 22
 }
+#2
+ def display_cohort(hash)
+hash.each {|key, value| puts " In #{key} there is #{value} students."}
+ end
+
+
+
+#3
+students[:cohort4] = 42
+
+
+#4
+students.keys
+
+
+#5
+start = 5
+
+students.each do |key, value|
+  start *= value
+  puts start
+end
+
+
+def product(array)
+  final = 0.0
+  array.each do |i|
+    final *= i
+  end
+  return final
+end
