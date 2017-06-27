c5d9ceae28fdae94e958dfab04499ae400d7871b
Kathleen <kathleenmcclay@gmail.com>
2017-06-27 06:29:36 -0400

    First commit exercise 5 complete

---
+++ exercise5.rb
@@ -0,0 +1,16 @@
+#part 1
+def greet_method(name)
+puts name.reverse
+end
+
+#part 2
+puts greet_method('Amanda')
+puts greet_method('Bob')
+puts greet_method('Shirley')
+puts greet_method('Sue')
+puts greet_method('Andy')
+
+#part 3
+def greet_method(name)
+puts name.reverse * 2
+end
